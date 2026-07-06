91387800dda0ea9b8d1ce65ba60a89b38256ed1c
mahdieh1992 <mohamadimahdieh70@gmail.com>
2026-07-06 11:58:04 +0330

    Implement school model

--- core/apps/school/models.py
+++ core/apps/school/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class School(models.Model):
+    """    school model stores information about schools
+    """
+    name = models.CharField(max_length= 150)
+    latitude = models.DecimalField(max_digits= 9, decimal_places= 6, blank= True, null= True)
+    longitude = models.DecimalField(max_digits= 9, decimal_places= 6, blank= True, null= True)
+    created_at = models.DateTimeField(auto_now_add= True)
+    updated_at = models.DateTimeField(auto_now= True)
+    
+    def __str__(self):
+        return self.name
