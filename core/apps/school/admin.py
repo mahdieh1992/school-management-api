7355ed53ef0a2bb4b985de29b7d590695439e7c0
mahdieh1992 <mohamadimahdieh70@gmail.com>
2026-07-08 18:08:13 +0330

    Admin configuration for the school model

--- core/apps/school/admin.py
+++ core/apps/school/admin.py
@@ -4,6 +4,9 @@ from .models import School
 
 @admin.register(School)
 class SchoolAdmin(admin.ModelAdmin):
+   """
+    Admin configuration for the School model.
+   """
    list_display = ("name", "latitude", "longitude","created_at", "updated_at")
    search_fields = ("name",)
    ordering = ("name",)
