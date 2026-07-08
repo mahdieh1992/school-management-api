b2e14d9feca32f0055f3714d85d97cb8d8bcf383
mahdieh1992 <mohamadimahdieh70@gmail.com>
2026-07-08 18:06:45 +0330

    [200~

--- core/apps/school/admin.py
+++ core/apps/school/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import School
 # Register your models here.
+
+@admin.register(School)
+class SchoolAdmin(admin.ModelAdmin):
+   list_display = ("name", "latitude", "longitude","created_at", "updated_at")
+   search_fields = ("name",)
+   ordering = ("name",)
+   
